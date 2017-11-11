babf5664323e764314f60c9ad1e56baa58367084
Eric Su, SMY <31977664+ladsSMY@users.noreply.github.com>
2017-11-11 14:51:09 +0800

    final check for homework

--- hw1/hw1_best.py
+++ hw1/hw1_best.py
@@ -61,22 +61,6 @@ repeat = 20000'''
 w = np.matmul(np.matmul(inv(np.matmul(x.transpose(),x)),x.transpose()),y)'''
 
 
-'''TRAINING
-x_t = x.transpose()
-s_gra = np.zeros(len(x[0]))
-
-for i in range(repeat):
-    hypo = np.dot(x,w)
-    loss = hypo - y
-    cost = np.sum(loss**2) / len(x)
-    cost_a  = math.sqrt(cost)
-    gra = np.dot(x_t,loss)
-    s_gra += gra**2
-    ada = np.sqrt(s_gra)
-    w = w - l_rate * gra/ada
-    print ('iteration: %d | Cost: %f  ' % ( i,cost_a))'''
-
-
 '''SAVE/READ MODEL
 np.save('model.npy',w)'''
 w = np.load('model.npy')
